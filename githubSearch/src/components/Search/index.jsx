a7a5953307a8a14732c96096eb65b0020adb6f2a
1023299082 <yw3367@columbia.edu>
2021-03-11 15:44:01 -0400

    use fetch

--- githubSearch/src/components/Search/index.jsx
+++ githubSearch/src/components/Search/index.jsx
@@ -3,21 +3,59 @@ import axios from "axios";
 import PubSub from 'pubsub-js'
 
 class Index extends Component {
-    search =()=>{
+    // search =()=>{
+    //     //get input
+    //     //连续解构赋值并重命名
+    //     const {keyWordNode:{value:keyWord}} = this
+    //     PubSub.publish('state', {isFirst:false,isLoading:true});
+    //     //send request
+    //     // axios.get(`https://api.github.com/search/users?q=${keyWord}`).then(
+    //     //     response =>{
+    //     //         PubSub.publish('state', {isLoading:false,users:response.data.items});
+    //     //     },
+    //     //     error=>{
+    //     //         PubSub.publish('state', {isLoading:false,err:error.message});
+    //     //     }
+    //     // )
+    //     fetch(`https://api.github.com/search/users?q=${keyWord}`).then(
+    //         response =>{
+    //             console.log("connect server success",response.json())
+    //             return response.json()
+    //         },
+    //         // error=>{
+    //         //     console.log("connect server failed",error)
+    //         //     return new Promise(()=>{})
+    //         // }
+    //     ).then(
+    //         response =>{
+    //             console.log("get data success",response)
+    //         },
+    //         // error =>{
+    //         //     console.log("get data failed",error)
+    //         // }
+    //     ).catch(
+    //         (error)=>{console.log(error)}
+    //     )
+    // }
+
+    search = async ()=> {
         //get input
         //连续解构赋值并重命名
-        const {keyWordNode:{value:keyWord}} = this
-        PubSub.publish('state', {isFirst:false,isLoading:true});
-        //send request
-        axios.get(`https://api.github.com/search/users?q=${keyWord}`).then(
-            response =>{
-                PubSub.publish('state', {isLoading:false,users:response.data.items});
-            },
-            error=>{
-                PubSub.publish('state', {isLoading:false,err:error.message});
-            }
-        )
+        const {keyWordNode: {value: keyWord}} = this
+        PubSub.publish('state', {isFirst: false, isLoading: true});
+        try{
+            const response = await fetch(`https://api.github.com/search/users?q=${keyWord}`)
+            const data = await response.json()
+            PubSub.publish('state', {isLoading:false,users:data.items});
+            console.log(data)
+        }catch (error){
+            console.log(error)
+            PubSub.publish('state', {isLoading:false,err:error.message});
+        }
     }
+
+
+
 
     render() {
         return (
@@ -30,6 +68,21 @@ class Index extends Component {
             </section>
         );
     }
+}
+
+function getNumber(){
+    var p = new Promise(function(resolve, reject){
+        setTimeout(function(){
+            var num = Math.ceil(Math.random()*10);
+            if(num<=5){
+                resolve(num);
+            }
+            else{
+                reject('Number too big');
+            }
+        }, 1000);
+    });
+    return p;
 }
 
 export default Index;
